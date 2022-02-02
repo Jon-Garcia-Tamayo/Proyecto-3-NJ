b3645dc24a3462f403fa930ffdbf4ad6c39f2dc9
Jon-Garcia-Tamayo <93374780+Jon-Garcia-Tamayo@users.noreply.github.com>
2022-02-02 10:16:15 +0100

    Terminada clase Utilidades

--- helper/utilidades.php
+++ helper/utilidades.php
@@ -1,9 +1,21 @@
 <?php
     class Utilidades
     {
-        public static function verificarControl()
+        public static function verificarLista($generos, $valorMenu)
         {
-            
+            foreach ($generos as $genero) {
+                if($genero == $valorMenu){
+                    echo ' selected = "selected"';
+                }
+            }
+        }
+
+        public static function verificarBotones($valor, $valorBoton)
+        {
+            if($valor == $valorBoton){
+                echo 'checked = "checked"';
+            }
+     
         }
     }
 ?>
