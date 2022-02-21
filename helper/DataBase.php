b41e4df8327952c3b730a5245c69dd95ee00fcbf
Naiara Borrega Oyaregui <93375347+Naiara-Borrega@users.noreply.github.com>
2022-02-21 20:52:05 +0100

    Añadidos comentarios

--- helper/DataBase.php
+++ helper/DataBase.php
@@ -1,15 +1,21 @@
 <?php
     include "helper/IDatabase.php";
 
+    /**
+     * Esta clase modela una base de datos para realizar una conexion.
+     */
     class DataBase implements IDatabase
     {
         private $conexion;
 
         public function __construct()
         {
-			
+            
         }
-
+        /**
+         * Este metodo realiza una conexion con la base de datos de MySQL
+         * Y establece la propiedad $conexion a un objeto PDO
+         */
         public function conectar()
         {
             $DB_SERVER = "localhost";
@@ -33,35 +39,50 @@
             }
         }
 
+        /**
+         * Establece la propiedad de conexion a null para desconectar la base de datos
+         */
         public function desconectar()
         {
             $this->conexion = null;
         }
 
+        /**
+         * Dada una sentencia de SQL se ejecuta en la base de datos manejando errores
+         * @return $resultado - Devuelve true o false dependiendo de si la sentencia se ha 
+         * ejecutado correctamente o no.
+         * @param $sql - Sentencia SQL a ejecutar.
+         */
         public function ejecutarSql($sql)
         {
-			$resultado;
-			try {
-				$resultado = $this->conexion->query($sql);
-			} catch (PDOException $e) {
-				return "Se ha producido un error al ejecutar la sentencia SQL";
-			}
-			return $resultado;
+            $resultado;
+            try {
+                $resultado = $this->conexion->query($sql);
+            } catch (PDOException $e) {
+                return "Se ha producido un error al ejecutar la sentencia SQL";
+            }
+            return $resultado;
         }
 
+        /**
+         * Dada una sentencia de SQL y una clausula se ejecuta en la base de datos manejando errores
+         * @return $resultado - Mensaje correspondiente a imprimir.
+         * @param $sql - Sentencia SQL a ejecutar.
+         * @param $args - Clausula a evaluar.
+         */
         public function ejecutarSqlActualizacion($sql, $args)
         {
-			try {
-				$resultado = $this->conexion->prepare($sql);
-				if ($resultado->execute()) {
-					return "Se ha realizado la operacion $args correctamente";
-				} else {
-					return "Se ha producido un error al ejecutar la sentencia SQL";
-				}
-			} catch (PDOException $e) {
-				return $e->getMessage();
-			}
-			
+            try {
+                $resultado = $this->conexion->prepare($sql);
+                if ($resultado->execute()) {
+                    return "Se ha realizado la operacion $args correctamente";
+                } else {
+                    return "Se ha producido un error al ejecutar la sentencia SQL";
+                }
+            } catch (PDOException $e) {
+                return $e->getMessage();
+            }
+            
         }
         
     }
